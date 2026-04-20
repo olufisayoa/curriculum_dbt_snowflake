ded41c469b9a5dee55f16735351e1cb650134e01
olufisayoa <olufisayo.akinmoju@the-etc.ac.uk>
2026-04-20 15:31:57 +0100

    feat(staging): fixed column reference error in dim_cohort model

--- models/marts/core/dim_cohort.sql
+++ models/marts/core/dim_cohort.sql
@@ -2,5 +2,5 @@
 SELECT 
          {{ dbt_utils.generate_surrogate_key([
          'TRIM(Cohort)'])}} AS "CohortKey",
-         "Cohort" AS "Cohort"
+         Cohort AS "Cohort"
 FROM {{ ref('int_cohort') }}
