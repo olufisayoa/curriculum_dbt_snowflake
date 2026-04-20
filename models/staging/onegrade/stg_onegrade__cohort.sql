5a3d4e69ce3f4c9e444b3804050d852efbc17b7f
olufisayoa <olufisayo.akinmoju@the-etc.ac.uk>
2026-04-20 15:29:10 +0100

    feat(staging): modified dim_cohort model

--- models/staging/onegrade/stg_onegrade__cohort.sql
+++
@@ -1,9 +0,0 @@
-
-WITH cohort AS (
-SELECT 
-{{ dbt_utils.generate_surrogate_key(['CohortName']) }} AS "CohortKey",
-CohortName AS "CohortName"
-FROM {{ source('Onegrade', 'ONEGRADE_COHORTLOOKUP') }}
-)
-
-SELECT * FROM cohort
