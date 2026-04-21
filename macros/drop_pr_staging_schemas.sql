252a4901137f865f5c7d72e16bfcc6de19d316c1
olufisayoa <olufisayo.akinmoju@the-etc.ac.uk>
2026-04-21 13:54:09 +0100

    Fix: drop pr schema macro

--- macros/drop_pr_staging_schemas.sql
+++ macros/drop_pr_staging_schemas.sql
@@ -5,6 +5,8 @@
 
   {% set drop_query = 'drop schema if exists ' ~ target.database ~ '.' ~ pr_schema ~ ' cascade' %}
 
-  {% do run_query(drop_query) %}
-  {{ log("Schema " ~ pr_schema ~ " dropped successfully", info=true) }}
+  {% if execute %}
+    {% do run_query(drop_query) %}
+    {{ log("Schema " ~ pr_schema ~ " dropped successfully", info=true) }}
+  {% endif %}
 {%- endmacro -%}
