41c1a2ebeb2edae5c7a34cf5710246a559303200
olufisayoa <olufisayo.akinmoju@the-etc.ac.uk>
2026-06-22 15:56:20 +0100

    feat(staging): add dim_subject model

---
+++ models/staging/manual/stg_manual_gcse_subject.sql
@@ -0,0 +1,12 @@
+
+WITH gcse_subject AS (
+    SELECT 1 AS gcse_subject_key, 'GCSE English' AS gcse_subject_name UNION ALL
+    SELECT 2, 'GCSE Maths' UNION ALL
+    SELECT 3, 'Not GCSE English / GCSE Maths'
+
+)
+
+SELECT 
+    gcse_subject_key::INT AS "gcse_subject_key",
+    gcse_subject_name::VARCHAR(100) AS "gcse_subject_name"
+FROM gcse_subject
