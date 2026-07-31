88678711f46072b1e2890ca1c6ea21a68140fce3
olufisayoa <olufisayo.akinmoju@the-etc.ac.uk>
2026-07-31 13:56:19 +0100

    feat(staging): fix missing SID column on register model

--- models/staging/prosolution/stg_prosolution__register.sql
+++ models/staging/prosolution/stg_prosolution__register.sql
@@ -2,5 +2,6 @@ SELECT
 "AcademicYearID" AS AcademicYearID,
 "RegisterID" AS RegisterID,
 "RegisterNo" AS RegisterNo,
-"Title" AS Title
+"Title" AS Title,
+"SID" AS SID
 FROM {{ source('ProSolution', 'PROSOLUTION_REGISTER') }}
