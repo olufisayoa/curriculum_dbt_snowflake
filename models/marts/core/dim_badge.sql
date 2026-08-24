d0e1e7262d069f8c864d53cec9aa48efbeac5ab4
olufisayoa <olufisayo.akinmoju@the-etc.ac.uk>
2026-08-24 11:31:37 +0100

    feat(staging): changed data type for IsObsolete column on dim_badge model

--- models/marts/core/dim_badge.sql
+++ models/marts/core/dim_badge.sql
@@ -3,6 +3,6 @@ SELECT
 B.BadgeName AS "BadgeName",
 BV.BadgeValueName AS "BadgeValueName",
 BV.BadgeCode AS "BadgeCode",
-BV.IsObsolete AS "IsObsolete"
+CAST(BV.IsObsolete AS BOOLEAN) AS "IsObsolete"
 FROM {{ ref('stg_promonitor__learnerinformation_badgevalue') }} BV
 JOIN {{ ref('stg_promonitor__learnerinformation_badge') }} B ON BV.BadgeID = B.BadgeID
