3368ff286272596c39fb28a9b29530cca1ac8c9c
olufisayoa <olufisayo.akinmoju@the-etc.ac.uk>
2026-08-24 15:42:46 +0100

    feat(staging): fix source import error on  stg_promonitor__learnerinformation_actionplan_actiontype model

--- models/staging/promonitor/stg_promonitor__learnerinformation_actionplan_actiontype.sql
+++ models/staging/promonitor/stg_promonitor__learnerinformation_actionplan_actiontype.sql
@@ -3,4 +3,4 @@ ActionType.ID AS ActionTypeID,
 ActionType.ActionDescription AS ActionTypeDescription,
 ActionType.OrderBy AS ActionTypeOrderBy,
 ActionType.IsObsolete AS IsObsolete
-FROM {{ source('Promonitor', 'PROMONITOR_LEANERINFORMATION_ACTIONPLAN_ACTIONTYPE') }} ActionType
+FROM {{ source('Promonitor', 'PROMONITOR_LEARNERINFORMATION_ACTIONPLAN_ACTIONTYPE') }} ActionType
