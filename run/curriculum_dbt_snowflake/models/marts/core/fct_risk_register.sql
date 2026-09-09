b8e412042924c3bf4b3e436f1bbc3675db08af9e
olufisayoa <olufisayoa@users.noreply.github.com>
2026-09-09 12:02:24 +0000

    deploy: dab1260c443ec85a53cd735c24db86da366b0d7d

---
+++ run/curriculum_dbt_snowflake/models/marts/core/fct_risk_register.sql
@@ -0,0 +1,14 @@
+
+  
+    
+
+create or replace transient table CURRICULUM_DB.core.fct_risk_register
+    
+    
+    
+    as (SELECT * FROM CURRICULUM_DB.int.int_risk_register
+    )
+;
+
+
+  
--- run/curriculum_dbt_snowflake/models/marts/core/intermediate/int_risk_register.sql
+++ run/curriculum_dbt_snowflake/models/marts/core/intermediate/int_risk_register.sql
@@ -1,4 +1,12 @@
-WITH prosolution_enrolments AS (
+
+  
+    
+
+create or replace transient table CURRICULUM_DB.int.int_risk_register
+    
+    
+    
+    as (WITH prosolution_enrolments AS (
     SELECT
     md5(cast(coalesce(cast(TRIM(o.AcademicYearID) as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(TRIM(sd.RefNo) as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(TRIM(o.Code) as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(TRIM(o.QualID) as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(CAST(e.StartDate AS DATE) as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(CAST(e.CompletionStatusID AS INTEGER) as TEXT), '_dbt_utils_surrogate_key_null_') as TEXT)) AS EnrolmentKey,
     md5(cast(coalesce(cast(TRIM(o.AcademicYearID) as TEXT), '_dbt_utils_surrogate_key_null_') as TEXT)) AS AcademicYearKey,
@@ -158,3 +166,8 @@ SELECT
       END AS DECIMAL(19,2)) AS "Value Added"
 FROM consolidated_enrolments e
 CROSS JOIN unpivot_helper h
+    )
+;
+
+
+  
