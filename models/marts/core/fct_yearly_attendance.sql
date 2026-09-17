6df47c787faa96e0edae26ab1b75d886c4518105
olufisayoa <olufisayo.akinmoju@the-etc.ac.uk>
2026-09-17 16:22:23 +0100

    feat(staging): removed fct_enrolment and fct_yearly attendance models

--- models/marts/core/fct_yearly_attendance.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    StudentKey AS "StudentKey",
-    AcademicYearKey AS "AcademicYearKey",
-    SUM(MrkPresent) as "MrkPresent",
-    SUM(MrkRequired) as "MrkRequired",
-    DIV0(SUM(MrkPresent), SUM(MrkRequired)) as "AttendanceRate"
-FROM {{ ref('int_attendance') }}
-GROUP BY 1, 2
